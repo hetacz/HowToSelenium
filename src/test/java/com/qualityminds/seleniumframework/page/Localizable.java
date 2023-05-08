04a5ac30313401db54c37637009bf9318a349a35
Hetacz <rafallovesworking@gmail.com>
2023-05-08 16:47:03 +0200

    strings amended

---
+++ src/test/java/com/qualityminds/seleniumframework/page/Localizable.java
@@ -0,0 +1,17 @@
+package com.qualityminds.seleniumframework.page;
+
+import com.qualityminds.seleniumframework.base.I18n;
+import com.qualityminds.seleniumframework.utils.StringLoader;
+
+public interface Localizable {
+
+    String HOME_URL = "home_url";
+    String HOME_TITLE = "home_title";
+    String AUTOMATION_URL = "automation_url";
+    String AUTOMATION_TITLE = "automation_title";
+    String H1_TEXT = "h1_text";
+
+    default String getString(I18n i18n, String key) {
+        return StringLoader.getInstance().strings(i18n).get(key).asText();
+    }
+}
