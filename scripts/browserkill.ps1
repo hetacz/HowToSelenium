515254e772ff6bead31df4430e5716cc035fe3e9
Hetacz <rafallovesworking@gmail.com>
2023-05-11 16:41:06 +0200

    browserkill.ps1

---
+++ scripts/browserkill.ps1
@@ -0,0 +1,6 @@
+﻿taskkill /f /im chrome.exe
+taskkill /f /im chromedriver.exe
+taskkill /f /im firefox.exe
+taskkill /f /im geckodriver.exe
+taskkill /f /im msedge.exe
+taskkill /f /im msedgedriver.exe
